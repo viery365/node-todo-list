a5fba12b4e0c11409dc350c8933e2b4668323b7c
viery365 <20521892+viery365@users.noreply.github.com>
2018-09-18 05:33:32 +0100

    Setup the "list" command

--- app.js
+++ app.js
@@ -1,63 +1,59 @@
-const fs = require('fs');
-const _ = require('lodash');
-const yargs = require('yargs');
+const fs = require("fs");
+const _ = require("lodash");
+const yargs = require("yargs");
 
-const tasks = require('./tasks');
-
-
+const tasks = require("./tasks");
 
 let argv = yargs
-    .command('add', 'Adds a task')
-    .command('list', 'Lists all tasks')
-     .help()
-    .argv;
-    
+  .command("add", "Adds a task")
+  .command("list", "Lists all tasks")
+  .command("read", "Read task")
+  .help().argv;
 
 let argvCommands = argv._;
 
-const command = argv._[0];
+const command = process.argv[2];
 
-if (command === 'add'){
+if (command === "add") {
+  argvCommands.shift();
 
-    argvCommands.shift();
+  const argvCommandsToString = argvCommands.join(" ");
 
-    const argvCommandsToString = argvCommands.join(' ');
-
-    if(argvCommands.length === 0){
-        console.log('You must provide a task');
-    }else {
-        var task = tasks.addTask(argvCommandsToString);
-        console.log('Task created');
-        tasks.logTask(task);
-    }
-   
-}else if(command === 'list'){
-    var allNotes = tasks.getAll();
-    if (allNotes.length > 0){
-        console.log(`Printing ${allNotes.length} note(s).`);
-        allNotes.forEach((note) => tasks.logNote(note));
-    }else{
-        console.log('No notes listed')
-    }
-
-}else if(command === 'read'){
-    var note = tasks.getNote(argv.title);
-    if (note){
-        console.log('Note found');
-        tasks.logNote(note);
-    }else{
-        console.log('Note not found');
-    }
-
-}else if(command === 'remove'){
-    var noteRemoved = tasks.removeNote(argv.title);
-    var message = (noteRemoved) ? 'Note was removed.' : 'Note not found';
-    console.log(message);
-    
-}else{
-    console.log('Command not recognized');
+  if (argvCommands.length === 0) {
+    console.log("You must provide a task");
+  } else {
+    var task = tasks.addTask(argvCommandsToString);
+    console.log("Task created");
+    console.log("---");
+    console.log(`Task: ${task.task}`);
+  }
+} else if (command === "list") {
+  if (argvCommands.length >= 2) {
+    console.log("///////////");
+    console.log("Next time just use 'list', it will be enough");
+    console.log("///////////");
+  }
+  var allTasks = tasks.getAll();
+  if (allTasks.length > 0) {
+    console.log(`Printing ${allTasks.length} task(s).`);
+    allTasks.forEach(task => {
+      console.log(task.task);
+    });
+  } else {
+    console.log("You have 0 tasks");
+  }
+} else if (command === "read") {
+  var note = tasks.getNote(argv.title);
+  if (note) {
+    console.log("Note found");
+    tasks.logNote(note);
+  } else {
+    console.log("Note not found");
+  }
+} else if (command === "remove") {
+  var noteRemoved = tasks.removeNote(argv.title);
+  var message = noteRemoved ? "Note was removed." : "Note not found";
+  console.log(message);
+} else {
+  console.log("Command not recognized");
 }
-
-
-
-
--- tasks.js
+++ tasks.js
@@ -1,65 +1,54 @@
-const fs = require('fs');
+const fs = require("fs");
 
 var fetchTasks = () => {
-    try{
-        //if file already exists
-        var tasksString = fs.readFileSync('tasks-db.json');
-        return JSON.parse(tasksString);
-    }catch(e){
-        //if there is no file yet
-        return [];
-    } 
-}
+  try {
+    //if file already exists
+    var tasksString = fs.readFileSync("tasks-db.json");
+    return JSON.parse(tasksString);
+  } catch (e) {
+    //if there is no file yet
+    return [];
+  }
+};
 
-var saveTask = (task) => {
-    fs.writeFileSync('tasks-db.json', JSON.stringify(task));
-}
+var saveTask = task => {
+  fs.writeFileSync("tasks-db.json", JSON.stringify(task));
+};
 
-var addTask = (task) => {
+var addTask = task => {
+  var tasks = fetchTasks();
 
-    var tasks = fetchTasks();
+  var task = {
+    task
+  };
 
-    var task = {
-        task
-    };
-
-    tasks.push(task);
-    saveTask(tasks);
-    return task;
-
+  tasks.push(task);
+  saveTask(tasks);
+  return task;
 };
 var getAll = () => {
-    return fetchNotes();
-}; 
-var getNote = (title) => {
-    var notes = fetchNotes();
-    var filteredNotes = notes.filter((note) => note.title === title);
-    return filteredNotes[0];
+  return fetchTasks();
 };
-var removeNote = (title) => {
-    //fetch notes
-    var notes = fetchNotes();
-    //filter notes, removing the one with the title of the argument
-    var filteredNotes = notes.filter((note) => note.title !== title);
-    //save new notes array
-    saveNotes(filteredNotes);
-
-    //check if a note was removed
-    return notes.length !== filteredNotes.length;
+var getNote = title => {
+  var notes = fetchNotes();
+  var filteredNotes = notes.filter(note => note.title === title);
+  return filteredNotes[0];
 };
+var removeNote = title => {
+  //fetch notes
+  var notes = fetchNotes();
+  //filter notes, removing the one with the title of the argument
+  var filteredNotes = notes.filter(note => note.title !== title);
+  //save new notes array
+  saveNotes(filteredNotes);
 
-var logTask = (task) => {
-    console.log('---');
-    console.log(`Task: ${task.task}`);
+  //check if a note was removed
+  return notes.length !== filteredNotes.length;
 };
 
 module.exports = {
-    addTask,
-    getAll,
-    getNote,
-    removeNote,
-    logTask
+  addTask,
+  getAll,
+  getNote,
+  removeNote
 };
-
-
-
